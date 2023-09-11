9a411a31a8c7b4c51a8e35c1a1199a69b7d23943
pietrolupi <pietro.lupi92@gmail.com>
2023-09-11 15:10:09 +0200

    [edit]:js

--- JS/script.js
+++ JS/script.js
@@ -7,10 +7,16 @@ for(let i = 0; i < 100; i++){
 
   if(!((i + 1) % 3) && !((i + 1) % 5)){
     box.classList.add('bg-red');
+    box.append('fizzbuzz');
   }else if(!((i + 1) % 5)){
     box.classList.add('bg-yellow');
+    box.append('buzz');
   }else if( !((i + 1) % 3 )){
     box.classList.add('bg-green');
-};
+    box.append('fizz');
+}else{
+    box.classList.add('bg-blu');
+    box.append(i + 1);
+}
 
 };
